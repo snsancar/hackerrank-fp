f8bcae1e96cd3304b36cc354a94a845cd27b7948
snsancar <sancar.sn@gmail.com>
2017-05-02 08:35:53 +0530

    new file:   src/com/sankar/StringOPermute.scala

---
+++ src/com/sankar/StringOPermute.scala
@@ -0,0 +1,12 @@
+package com.sankar
+
+object StringOPermute {
+  
+  def permute(s: String): String = {
+    s.sliding(2, 2).map(_.reverse).mkString
+  }
+  def main(args: Array[String]) {
+    val n = readInt()
+    (0 until n).foreach(_ => println(permute(readLine())))
+  }
+}
