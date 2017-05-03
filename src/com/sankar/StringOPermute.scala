001a4a6c7adc04d54484cb534e55dcaaea73cee7
snsancar <sancar.sn@gmail.com>
2017-05-03 22:38:18 +0530

    modified:   src/com/sankar/StringOPermute.scala

--- src/com/sankar/StringOPermute.scala
+++ src/com/sankar/StringOPermute.scala
@@ -3,7 +3,8 @@ package com.sankar
 object StringOPermute {
   
   def permute(s: String): String = {
-    s.sliding(2, 2).map(_.reverse).mkString
+    //s.sliding(2, 2).map(_.reverse).mkString
+    s.grouped(2).map { x => x.reverse }.mkString
   }
   def main(args: Array[String]) {
     val n = readInt()
